e99f50422d6225accd4973dcdc62910d85bb5d7b
Josh Hatcher <jbhtcher@memphis.edu>
2018-10-03 23:54:23 -0500

    Add classes for each new file in the hierarchy.

---
+++ Roomies/Models/Apartment/Groceries/GroceryManager.swift
@@ -0,0 +1,11 @@
+//
+//  GroceryManager.swift
+//  Roomies
+//
+//  Created by Josh Hatcher on 10/3/18.
+//  Copyright © 2018 Keaton Burleson. All rights reserved.
+//
+
+import Foundation
+
+class GroceryManager {}
